6a4796a44c4f2222db043eb4b9063b8d150d542c
mrRestyling <sergeev_e_k@mail.ru>
2024-08-22 15:43:55 +0300

    addAtomicAndGenerics

--- 13_Slice(pro)/draft.go
+++ 13_Slice(pro)/draft.go
@@ -8,24 +8,24 @@ func main() {
 
 func elements() {
 
-	slice := []int{1, 2, 3, 4, 5}
+	slice := []int{1, 2, 3, 4, 5} //
 	i := 2
 
-	fmt.Println(slice)
+	// fmt.Println(slice)
 
-	withSlice := slice[:i+copy(slice[i:], slice[i+1:])]
-	// slice[:(2+2)]
-	// copy(slice[3,4,5],slice[4,5])
-	// slice = [1,2,4,5,5]
-	// withSlice = [1,2,4,5]
+	// withSlice := slice[:i+copy(slice[i:], slice[i+1:])]
 
 	fmt.Println(slice)
+	// fmt.Println(withSlice)
 
-	fmt.Println(withSlice)
-
+	// 1) 3 4 5
+	// 2) 4 5
+	// slice 1 2 4 5 5
 	slice = append(slice[:i], slice[i+1:]...)
-
-	fmt.Println()
 	fmt.Println(slice)
-
 }
+
+// slice[:(2+2)]
+// copy(slice[3,4,5],slice[4,5])
+// slice = [1,2,4,5,5]
+// withSlice = [1,2,4,5]
